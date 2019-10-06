ec9ea7d3b8a6456234c04ffbf409b233dcdfc776
pritesh-patel <pritesh-patel@outlook.com>
2019-10-06 16:52:25 -0700

    fix file pathing and health checks

--- example/hello-world/server/helloworld/helloworld.go
+++ example/hello-world/server/helloworld/helloworld.go
@@ -2,7 +2,8 @@ package helloworld
 
 import (
 	"context"
-	api "github.com/yourrepo/hello-world-idl/gen/go/helloworld"
+	health_api "github.com/yourrepo/hello-world-idl/gen/go/health"
+	//api "github.com/yourrepo/hello-world-idl/gen/go/helloworld"
 )
 
 type HelloworldServer struct {
@@ -11,4 +12,11 @@ type HelloworldServer struct {
 
 func NewHelloworldServer() *HelloworldServer {
 	return &HelloworldServer{}
+}
+
+func (s *HelloworldServer) Check(ctx context.Context, req *health_api.HealthCheckRequest) (*health_api.HealthCheckResponse, error) {
+	resp := &health_api.HealthCheckResponse{
+		Status: health_api.HealthCheckResponse_SERVING,
+	}
+	return resp,nil
 }
