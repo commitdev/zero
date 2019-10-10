efec50fc980a0372abde657c5cdeaaf91d378fc6
William Casarin <jb55@jb55.com>
2019-10-10 16:53:41 -0700

    cmd: create standalone Init function
    This will allow us to call this from within a test
    
    Signed-off-by: William Casarin <jb55@jb55.com>

--- cmd/commit0.go
+++ cmd/commit0.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"github.com/commitdev/commit0/templator"
+	"github.com/gobuffalo/packr/v2"
 	"github.com/spf13/cobra"
 	"os"
 )
@@ -17,8 +18,13 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-func Execute(templates *templator.Templator) {
-	Templator = templates
+func Init() {
+	templates := packr.New("templates", "../templates")
+	Templator = templator.NewTemplator(templates)
+}
+
+func Execute() {
+	Init()
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"github.com/commitdev/commit0/cmd"
-	"github.com/commitdev/commit0/templator"
-	"github.com/gobuffalo/packr/v2"
 )
 
 func main() {
-	templates := packr.New("templates", "./templates")
-	templator := templator.NewTemplator(templates)
-	cmd.Execute(templator)
+	cmd.Execute()
 }
