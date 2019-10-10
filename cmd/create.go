5f87969d4e42f8d5d02a387a774e73c1e8903163
William Casarin <jb55@jb55.com>
2019-10-10 16:56:28 -0700

    moveonly: create standlone function for Create
    Signed-off-by: William Casarin <jb55@jb55.com>

--- cmd/create.go
+++ cmd/create.go
@@ -13,6 +13,34 @@ func init() {
 	rootCmd.AddCommand(createCmd)
 }
 
+func Create(projectName string) {
+	rootDir := fmt.Sprintf("./%v", projectName)
+
+	log.Printf("Creating project %s.", projectName)
+
+	err := os.Mkdir(rootDir, os.ModePerm)
+	if os.IsExist(err) {
+		log.Fatalf("Directory %v already exists! Error: %v", projectName, err)
+	} else if err != nil {
+		log.Fatalf("Error creating root: %v ", err)
+	}
+
+	commit0ConfigPath := fmt.Sprintf("%v/commit0.yml", rootDir)
+
+	f, err := os.Create(commit0ConfigPath)
+	if err != nil {
+		log.Printf("Error creating commit0 config: %v", err)
+	}
+	Templator.Commit0.Execute(f, projectName)
+
+	gitIgnorePath := fmt.Sprintf("%v/.gitignore", rootDir)
+	f, err = os.Create(gitIgnorePath)
+	if err != nil {
+		log.Printf("Error creating commit0 config: %v", err)
+	}
+	Templator.GitIgnore.Execute(f, projectName)
+}
+
 var createCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Create new project with provided name.",
@@ -23,30 +51,6 @@ var createCmd = &cobra.Command{
 
 		projectName := args[0]
 
-		rootDir := fmt.Sprintf("./%v", projectName)
-
-		log.Printf("Creating project %s.", projectName)
-
-		err := os.Mkdir(rootDir, os.ModePerm)
-		if os.IsExist(err) {
-			log.Fatalf("Directory %v already exists! Error: %v", projectName, err)
-		} else if err != nil {
-			log.Fatalf("Error creating root: %v ", err)
-		}
-
-		commit0ConfigPath := fmt.Sprintf("%v/commit0.yml", rootDir)
-
-		f, err := os.Create(commit0ConfigPath)
-		if err != nil {
-			log.Printf("Error creating commit0 config: %v", err)
-		}
-		Templator.Commit0.Execute(f, projectName)
-
-		gitIgnorePath := fmt.Sprintf("%v/.gitignore", rootDir)
-		f, err = os.Create(gitIgnorePath)
-		if err != nil {
-			log.Printf("Error creating commit0 config: %v", err)
-		}
-		Templator.GitIgnore.Execute(f, projectName)
+		Create(projectName)
 	},
 }
