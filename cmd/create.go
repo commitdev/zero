aee97da3fd8c37a6f5611e35bb1e3b99191f49e7
William Casarin <jb55@jb55.com>
2019-10-10 16:57:46 -0700

    cmd: add outDir to Create
    This will allow us to generate projects into any directory we would like.
    
    We also:
    
      - replace spome Sprintfs with path.Join
      - Use MkdirAll instead of MkDir
      - Return the project directory
    
    Signed-off-by: William Casarin <jb55@jb55.com>

--- cmd/create.go
+++ cmd/create.go
@@ -1,9 +1,9 @@
 package cmd
 
 import (
-	"fmt"
 	"log"
 	"os"
+	"path"
 
 	"github.com/spf13/cobra"
 )
@@ -13,19 +13,19 @@ func init() {
 	rootCmd.AddCommand(createCmd)
 }
 
-func Create(projectName string) {
-	rootDir := fmt.Sprintf("./%v", projectName)
-
+func Create(projectName string, outDir string) string {
+	rootDir := path.Join(outDir, projectName)
 	log.Printf("Creating project %s.", projectName)
+	err := os.MkdirAll(rootDir, os.ModePerm)
 
-	err := os.Mkdir(rootDir, os.ModePerm)
 	if os.IsExist(err) {
 		log.Fatalf("Directory %v already exists! Error: %v", projectName, err)
 	} else if err != nil {
 		log.Fatalf("Error creating root: %v ", err)
 	}
 
-	commit0ConfigPath := fmt.Sprintf("%v/commit0.yml", rootDir)
+	commit0ConfigPath := path.Join(rootDir, "commit0.yml")
+	log.Printf("%s", commit0ConfigPath)
 
 	f, err := os.Create(commit0ConfigPath)
 	if err != nil {
@@ -33,12 +33,14 @@ func Create(projectName string) {
 	}
 	Templator.Commit0.Execute(f, projectName)
 
-	gitIgnorePath := fmt.Sprintf("%v/.gitignore", rootDir)
+	gitIgnorePath := path.Join(rootDir, ".gitignore")
 	f, err = os.Create(gitIgnorePath)
 	if err != nil {
 		log.Printf("Error creating commit0 config: %v", err)
 	}
 	Templator.GitIgnore.Execute(f, projectName)
+
+	return rootDir
 }
 
 var createCmd = &cobra.Command{
@@ -51,6 +53,6 @@ var createCmd = &cobra.Command{
 
 		projectName := args[0]
 
-		Create(projectName)
+		Create(projectName, "./")
 	},
 }
