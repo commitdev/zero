e45bf63f9f9d482c880d235428a09ff20aeac8c1
Shah Newaz Khan <info@shahnewazkhan.ca>
2019-10-17 21:53:39 -0700

    Removed workstation ip whitelist for eks vpc

--- templates/kubernetes/teraform/eks-cluster.tf
+++ templates/kubernetes/teraform/eks-cluster.tf
@@ -61,16 +61,6 @@ resource "aws_security_group_rule" "demo-cluster-ingress-node-https" {
   type                     = "ingress"
 }
 
-resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
-  cidr_blocks       = ["${local.workstation-external-cidr}"]
-  description       = "Allow workstation to communicate with the cluster API Server"
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.demo-cluster.id}"
-  to_port           = 443
-  type              = "ingress"
-}
-
 resource "aws_eks_cluster" "demo" {
   name     = "${var.cluster-name}"
   role_arn = "${aws_iam_role.demo-cluster.arn}"
--- templates/kubernetes/teraform/providers.tf
+++ templates/kubernetes/teraform/providers.tf
@@ -11,9 +11,3 @@ provider "aws" {
 data "aws_region" "current" {}
 
 data "aws_availability_zones" "available" {}
-
-# Not required: currently used in conjuction with using
-# icanhazip.com to determine local workstation external IP
-# to open EC2 Security Group access to the Kubernetes cluster.
-# See workstation-external-ip.tf for additional information.
-provider "http" {}
