a1810e25ef20388db628a86ab2912c9d13683483
Shah Newaz Khan <info@shahnewazkhan.ca>
2019-11-06 00:02:51 -0800

    WIP: added secret.yaml creation and prompts | TODO: Validate aws keys

--- internal/generate/kubernetes/generate.go
+++ internal/generate/kubernetes/generate.go
@@ -1,6 +1,7 @@
 package kubernetes
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -10,7 +11,14 @@ import (
 
 	"github.com/commitdev/commit0/internal/config"
 	"github.com/commitdev/commit0/internal/templator"
+	"github.com/manifoldco/promptui"
+	"gopkg.in/yaml.v2"
 )
+
+type Secrets struct {
+	AwsAccessKeyID     string
+	AwsSecretAccessKey string
+}
 
 func Generate(t *templator.Templator, cfg *config.Commit0Config, wg *sync.WaitGroup, pathPrefix string) {
 	data := templator.GenericTemplateData{*cfg}
@@ -19,14 +27,114 @@ func Generate(t *templator.Templator, cfg *config.Commit0Config, wg *sync.WaitGr
 
 func Execute(config *config.Commit0Config, pathPrefix string) {
 	if config.Infrastructure.AWS.EKS.Deploy {
+		log.Println("Preparing aws environment...")
+
+		dir, err := os.Getwd()
+		if err != nil {
+			log.Fatalf("Getting working directory failed: %v\n", err)
+		}
+
+		if fileExists(fmt.Sprintf("%s/secrets.yaml", dir)) {
+			log.Println("secrets.yaml exists ...")
+		} else {
+			awsSecrets := promptCredentials()
+			writeSecrets(awsSecrets)
+		}
+
 		log.Println("Planning infrastructure...")
 		execute(exec.Command("terraform", "init"), pathPrefix)
 		execute(exec.Command("terraform", "plan"), pathPrefix)
 	}
 }
 
-func execute(cmd *exec.Cmd, pathPrefix string) {
+func writeSecrets(s Secrets) {
+	secretsYaml, err := yaml.Marshal(&s)
+
+	fmt.Printf("SECRETS: %v", string(secretsYaml))
+
+	if err != nil {
+		log.Fatalf("error: %v", err)
+		panic(err)
+	}
+
 	dir, err := os.Getwd()
+
+	if err != nil {
+		log.Fatalf("Getting working directory failed: %v\n", err)
+		panic(err)
+	}
+
+	secretsFile := fmt.Sprintf("%s/secrets.yaml", dir)
+	// err = ioutil.WriteFile(secretsFile, []byte(secretsYaml), 0644)
+	f, err := os.Create(secretsFile)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+		panic(err)
+	}
+
+	defer f.Close()
+
+	n3, err := f.WriteString(string(secretsYaml))
+	f.Sync()
+	log.Printf("wrote %d bytes to %v", n3, secretsFile)
+
+}
+
+func promptCredentials() Secrets {
+
+	// validate := func(input string) error {
+	// 	var awsAccessKeyIDPat = regexp.MustCompile(`[A-Z0-9]{20}(?![A-Z0-9])`)
+	// 	if !awsAccessKeyIDPat.MatchString(input) {
+	// 		return errors.New("Invalid number")
+	// 	}
+	// 	return nil
+	// }
+
+	accessKeyIDPrompt := promptui.Prompt{
+		Label: "Aws Access Key ID",
+	}
+
+	accessKeyIDResult, err1 := accessKeyIDPrompt.Run()
+
+	if err1 != nil {
+		log.Fatalf("Prompt failed %v\n", err1)
+		panic(err1)
+	}
+
+	// awsSecrets.aws.awsAccessKeyID = accessKeyIDResult
+
+	secretAccessKeyPrompt := promptui.Prompt{
+		Label: "Aws Access Key ID",
+	}
+
+	secretAccessKeyResult, err2 := secretAccessKeyPrompt.Run()
+
+	if err2 != nil {
+		log.Fatalf("Prompt failed %v\n", err2)
+		panic(err2)
+	}
+
+	// awsSecrets.aws.awsSecretAccessKey = secretAccessKeyResult
+	awsSecrets := Secrets{
+		accessKeyIDResult,
+		secretAccessKeyResult,
+	}
+
+	return awsSecrets
+
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func execute(cmd *exec.Cmd) {
+	dir, err := os.Getwd()
+
 	if err != nil {
 		log.Fatalf("Getting working directory failed: %v\n", err)
 	}
