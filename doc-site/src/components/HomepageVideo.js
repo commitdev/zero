af6efd2d244dfdc7d646ccc43fab00fd97db9ea6
David Cheung <davidcheung@live.ca>
2021-07-23 15:47:37 -0400

    doc-site: update landing page content

--- doc-site/src/components/HomepageVideo.js
+++ doc-site/src/components/HomepageVideo.js
@@ -3,6 +3,6 @@ import styles from './HomepageVideo.module.scss';
 
 export default function FeatureVideo () {
 return <div className={styles.video}>
-    <iframe width="850" height="450" src="https://www.youtube.com/embed/IZ45Wm3wN7s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="850" height="450" src="https://www.youtube.com/embed/6rBM8L8dz4A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 }
